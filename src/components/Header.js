4c10603b27a3e0da2f605c5cf3cab8b2c47385b4
Ivan Monichev <ivan@monichev.ru>
2022-06-20 00:41:51 +0300

    feat: add nav menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,19 @@ import React from "react";
 function Header() {
     return (
         <header className="header">
-          <img src={logo} alt="Логотип Mesto Russia" className="header__logo" />
-          <button type="button" className="header__burger header__burger_is-active">
-            <span className="header__burger-line header__burger-line_is-active"/>
-          </button>
-          <nav className="header__menu header-nav">
+          <div className="header__body">
+            <img src={logo} alt="Логотип Mesto Russia" className="header__logo"/>
+            <button type="button" className="header__burger header__burger_is-active">
+              <span className="header__burger-line header__burger-line_is-active"/>
+            </button>
+          </div>
+          <nav className="header__menu header-nav header-nav_is-active">
             <ul className="header-nav__list">
               <li className="header-nav__item">email@mail.ru</li>
               <li className="header-nav__item"><a href="#" className="header-nav__link link-fade button-action">Выйти</a></li>
             </ul>
           </nav>
         </header>
-
     );
 }
 
