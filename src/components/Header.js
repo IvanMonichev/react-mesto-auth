710552c2ab3ba1c8010bee0b406514743fcef4e7
Ivan Monichev <ivan@monichev.ru>
2022-06-23 00:27:40 +0300

    fix: update breakpoints

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,19 @@ import React from "react";
 import {Switch, Route, Link} from "react-router-dom";
 
 function Header({onExit, email}) {
+
+  const burgerRef = React.useRef();
+  const burgerLineRef = React.useRef();
+  const headerMenuRef = React.useRef();
+
+
+  const handleToggleActiveNav = () => {
+    burgerRef.current.classList.toggle('header__burger_is-active');
+    burgerLineRef.current.classList.toggle('header__burger-line_is-active');
+    headerMenuRef.current.classList.toggle('header-nav_is-active');
+  }
+
+
   return (
     <header className="header">
       <Switch>
@@ -30,11 +43,11 @@ function Header({onExit, email}) {
           <div className="header__body">
             <img src={logo} alt="Логотип Mesto Russia" className="header__logo"/>
 
-            <button type="button" className="header__burger header__burger_is-active">
-              <span className="header__burger-line header__burger-line_is-active"/>
+            <button ref={burgerRef} type="button" className="header__burger" onClick={handleToggleActiveNav}>
+              <span ref={burgerLineRef} className="header__burger-line"/>
             </button>
           </div>
-          <nav className="header__menu header-nav header-nav_atop header-nav_is-active">
+          <nav ref={headerMenuRef} className="header__menu header-nav header-nav_atop">
             <ul className="header-nav__list">
               <li className="header-nav__item">{email}</li>
               <li className="header-nav__item">
